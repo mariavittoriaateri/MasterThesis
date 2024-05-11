9b8cfd0fbba495ec33ea46fe0b4642517d9b6606
Maria Vittoria Ateri <104144247+mariavittoriaateri@users.noreply.github.com>
2024-05-11 17:11:32 +0200

    Update run_me.sh

--- IRNet/preprocess/run_me.sh
+++ IRNet/preprocess/run_me.sh
@@ -8,12 +8,12 @@ table_data=$2
 output=$3
 
 echo "Start download NLTK data"
-python download_nltk.py
+python ./preprocess/download_nltk.py
 
 echo "Start process the origin Spider dataset"
-python data_process.py --data_path ${data} --table_path ${table_data} --output "process_data.json"
+python ./preprocess/data_process.py --data_path ${data} --table_path ${table_data} --output "process_data.json"
 
 echo "Start generate SemQL from SQL"
-python sql2SemQL.py --data_path process_data.json --table_path ${table_data} --output ${output} # in the original script, it states ${data} but I believe it is a typo. 
+python ./preprocess/sql2SemQL.py --data_path process_data.json --table_path ${table_data} --output ${output} # in the original script, it states ${data} but I believe it is a typo. 
 
 rm process_data.json
