13953bfb6e76828a3b2b2cab7834a1041dfb7641
Maria Vittoria Ateri <104144247+mariavittoriaateri@users.noreply.github.com>
2024-07-30 13:48:52 +0200

    Update sql_execution_output_exp3_smbop.sql

--- Exp3/sql_execution_output_exp3_smbop.sql
+++ Exp3/sql_execution_output_exp3_smbop.sql
@@ -1,1 +1,11 @@
+Error executing query: SELECT node.node_id , prosody_nodes.word FROM prosody_nodes JOIN node ON prosody_nodes.node_id = node.node_id WHERE node.node_type = 'syntactic leaf' INTERSECT SELECT prosody_nodes.word FROM prosody_nodes JOIN node ON prosody_nodes.node_id = node.node_id WHERE node.node_type = 'syntactic leaf';
+Error message: SELECTs to the left and right of INTERSECT do not have the same number of result columns
 
+Results for query: SELECT prosody_nodes.word FROM prosody_nodes WHERE prosody_nodes.word LIKE '%nuclear%';
+Nothing returned
+
+Results for query: SELECT sentences.sentence_array FROM sentences WHERE sentences.sentence_length<(SELECT MAX( sentences.sentence_length ) FROM sentences WHERE sentences.sentence_length LIKE '%DPs%');
+Nothing returned
+
+Results for query: SELECT prosody_nodes.word FROM prosody_nodes JOIN syntax_graph ON prosody_nodes.graph_id = syntax_graph.graph_id;
+Nothing returned
